2034c6ad4d748939e23210618b212eac2e87fa35
Carrabina <zakopako@mail.com>
2022-05-07 02:00:55 +0200

    * returnPassage bug fixed * nicer stat change notifications * slave array updated when npc changes stats and it's a slave * typos fixed with new spell checker installed on VSCode * Able to play a female protagonist. * Removed unused passages for interactions in basement

--- ts/npcInteraction.ts
+++ ts/npcInteraction.ts
@@ -20,7 +20,8 @@ interface NpcInteractionCollection {
 Macro.add("openNpcInteraction", {
   handler: function () {
     let variables = Variables();
-    variables.returnPassage = SugarCube.State.passage;
+    if (SugarCube.State.passage != "npcInteraction")
+      variables.returnPassage = SugarCube.State.passage;
     variables.npcInteractionRoute = this.args[0];
     SugarCube.State.display("npcInteraction");
   },
@@ -40,10 +41,19 @@ Macro.add("npcInteraction", {
     if (interaction && interaction.npcStats) {
       result += "@@color:yellow;";
       interaction.npcStats.forEach((change) => {
-        result += " " + change;
+        result +=
+          " " +
+          (change.charAt(0).toUpperCase() + change.slice(1)).replace(
+            /(\B[A-Z])/g,
+            " $1"
+          );
         let match = /(\w+)([+-]\d+)/.exec(change);
-        let varPath = "SugarCube.State.variables." + match[1];
+        let varPath = "variables().npc." + match[1];
         eval(`${varPath}=Math.min(100, Math.max(0, ${varPath + match[2]}))`);
+        var slaveIndex = (variables() as any).npc.index;
+        if (slaveIndex != undefined)
+          //TODO: might not be the best way to determine if it's a slave
+          (variables() as any).slaves[slaveIndex][match[1]] = eval(varPath);
       });
       result += "@@\n";
     }
@@ -112,7 +122,6 @@ Macro.add("npcInteraction", {
       }
       result += `\n<<keyOption '${stopOptionText}' $returnPassage ${emoji}>>`;
     }
-    console.info(result);
     $(document.createElement("span")).wiki(result).appendTo(this.output);
   },
 });
@@ -121,7 +130,7 @@ window.Interactions = {
     defaultStopOption: "✋ Leave $npc.pronoun alone",
     contents: `You approach $npc.name.
     <<include npcStats>>
-    $npc.genPronoun nerviously looks at you with $npc.possesive $npc.eyeColor eyes.`,
+    $npc.genPronoun nervously looks at you with $npc.possesive $npc.eyeColor eyes.`,
     options: {
       pushDown: {
         optionText: "👇 Push $npc.pronoun down",
@@ -133,7 +142,7 @@ window.Interactions = {
           strip: {
             optionText: "👌 Strip $npc.pronoun naked",
             contents:
-              "You take off all $npc.possesive clothes leaving $npc.name completely naked in front of you.",
+              "You take off all $npc.possesive clothes leaving $npc.name completely naked in front of you.\nYou admire $npc.possesive nice body 👀.",
             next: {
               penetrate: {
                 playerRequeriments: ["gender=male"],
@@ -143,6 +152,15 @@ window.Interactions = {
                 npcStats: ["fear+50"],
                 showNpcStats: true,
               },
+              rubPussies: {
+                playerRequeriments: ["gender=female"],
+                npcRequeriments: ["gender=female"],
+                optionText: "Trib pussies together.",
+                contents: `You open her legs and you start rubbing your pussy against hers.\nShe doesn't seem to dislike it.`,
+                npcStats: ["fear-5", "lewd+2"], //TODO add percentage
+                //next: window.Interactions.slave.options.pushDown.next.strip.next,
+              },
+              //TODO
             },
           },
         },

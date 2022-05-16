948ee94289101f069b0cfb502db483c4deee4346
Carrabina <zakopako@mail.com>
2022-05-17 01:49:56 +0200

    * Extracted shop css * FreedomWish decreasing a little each day

--- ts/time.ts
+++ ts/time.ts
@@ -57,8 +57,9 @@ class Now {
     for (let index = 0; index < amount; index++) {
       slaves.forEach((slave) => {
         slave.horny = false;
-        slave.fear = Math.max(0, slave.fear - 5);
+        slave.fear = Math.max(0, slave.fear - 10);
         slave.hunger = Math.min(100, slave.hunger + 10);
+        slave.freedomWish = Math.min(100, slave.freedomWish - 5);
       });
       player.lust = Math.min(100, player.lust + 10);
     }

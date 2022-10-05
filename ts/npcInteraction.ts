6a1d9b5639bb454b45db202ff422f0a8d3820bd4
Carrabina <zakopako@mail.com>
2022-10-05 18:23:29 +0200

    Added more comments explaining fields usage

--- ts/npcInteraction.ts
+++ ts/npcInteraction.ts
@@ -1,36 +1,90 @@
 type NpcInteractionOptions = Record<string, NpcInteraction>;
+//Describes an option that once selected will display a passage and optionally affect the relationship between the player and an NPC
 interface NpcInteraction {
+  //If this function is specified it will be called in order to know if this interaction can be shown in a list of options
   canBeShown?: () => boolean;
+  //A list of requirements that the player needs to comply in order to show this option
   playerRequirements?: string[];
+  //A list of requirements that the npc needs to comply in order to show this option
+  //Examples: lust>10, fear<=50, aroused, !analVirgin, love=100, status=servant
   npcRequirements?: string[];
+  //A list of items that the player needs to have in order to show this option
   inventoryRequirements?: string[];
+  //A list of items that the current location needs to comply (location name or items present in the location)
   locationRequirements?: string[];
+  //A list of requirements that the settings need to comply in order to show this option
   settingsRequirements?: string[];
+  //The text in the option (before selecting the option).
+  //if an emoji followed by a space is prepended it will be used as the emoji parameter in the keyOption or keyAction macro.
+  //Sugarcube markup can be written on it.
   optionText: string;
+  //The contents that will be shown as a passage once the option is selected.
+  //The scenery of the last location will be shown.
+  //Sugarcube markup can be written on it and it will be processed first (before any npc stat change)
   contents: string;
+  //A list of npc stat changes that will be executed after the option is selected and the contents processed.
+  //The changes will also be presented to the player in the passage.
+  //Optionally, a function to generate the list can be provided.
+  //"fear-10" will subtract exactly 10 to the npc's fear
+  //"lust+10%" will add a 10% of the current value
+  //"love%+40" will add a 40% of what is needed to reach 40 in love (fairmath) but at least will add 1
+  //"+aroused" will set $npc.aroused to true and "-aroused" will set it to false
   npcStats?: string[] | ((npc: Npc) => string[]);
+  //A list of player stat changes that will be executed after the option is selected and the contents processed.
   playerStats?: string[];
+  //The next options to show in the passage after this option is selected.
+  //Optionally, a function returning the options can be provided instead.
+  //But be careful, the engine can lose the path to the interaction to show if the function does not return the option that leads to the current interaction and produce an error.
+  //If that happens use altOptions and baseRoute instead.
+  //After all those options the stop option will be shown unless specified not to (see below)
   next?: NpcInteractionOptions | (() => NpcInteractionOptions);
+  //The text to show in the option to end the interaction with the NPC.
+  //Once selected the player will be redirected to the passage where the interaction started with the openNpcInteraction macro.
+  //If not specified the defaultStopOption in the parent interaction collection will be used.
+  //If it's set to false the stop option will not be shown.
   stopOption?: string | false;
+  //If set to true the npc's stats will be added to the passage. Useful when npc stats changed.
   showNpcStats?: boolean;
+  //The amount of minutes that will pass after selecting this option. It's also shown after the option text.
   minutesCost?: number;
+  //A function to alter the options specified in the next field above in case you need a special set of options when this specific interaction is shown.
   altOptions?: (
     npc: Npc,
     current: NpcInteractionOptions
   ) => NpcInteractionOptions;
+  //If specified the function is used to alter the minutesCost field when the option is selected. But the option will still show the minutes stated in minutesCost,
   altMinutes?: (current: number) => number;
+  //If specified changes the base route to the options on this interaction.
+  //For instance: If the function returns "slave.pushDown" and the options are named option1 and option2 the options will lead to the interactions "slave.pushDown.option1" and "slave.pushDown.option2"
   baseRoute?: (npc: Npc) => string;
+  //If specified, forces to show or not this interaction if there's no options available for the player to select other than a "back" option or the stop option.
+  //If not specified options without sub-options are only hidden if hideEmptyOptions on the parent collection is set to true.
   showIfEmpty?: boolean;
+  //If set to true this interaction will not open in a new passage and execute the Sugarcube markup in the contents field instead once this option is selected.
   action?: boolean;
+  //If specified, overrides timeIncreaseNpcHunger from the parent collection.
   timeIncreaseNpcHunger?: boolean | undefined;
 }
 interface NpcInteractionCollection {
+  //The first set of options in the collection (see NpcInteraction.next)
   options: NpcInteractionOptions | (() => NpcInteractionOptions);
+  //The contents of the first passage.
   contents: string;
+  //Every interaction in this interaction tree will have an option to exit the interaction and return to the passage that opened the first interaction.
+  //This field indicates the text on that option. If not specified the text "🔙 Return" will be used.
+  //If it's set to false, the stop option will never show unless specified in a sub-interaction.
   defaultStopOption?: string | false;
+  //If this option is set to true all the interactions in this collection with minutesCost specified will increase NPC hunger unless it's overridden by the same field in that interaction.
   timeIncreaseNpcHunger?: boolean;
+  //Hide all options in the entire collection that do not have any options to show other than a "back" option or the stop option.
+  //Can be overridden by the showIfEmpty field in a specific interaction.
   hideEmptyOptions?: boolean;
 }
+//Redirects to the npcInteraction passage showing an interaction with a NPC.
+//Usage: <<openNpcInteraction <interactionRoute>[ npcUid]>>
+//interactionRoute is the name of the interaction collection (that must be added to window.Interactions array) followed by the name of the selected options
+//npcUid is the target npc's UID and it's optional. If not specified the existing Npc in the $npc story variable will be used.
+//Example: <<keyAction directPiston>><<openNpcInteraction 'slave.pushDown.strip.pushDickAnus.ram.fast' $slaves[0].uid>><</keyAction>>
 Macro.add("openNpcInteraction", {
   handler: function () {
     let variables = Variables();
@@ -85,6 +139,8 @@ const checkCanBeShown = (option: NpcInteraction) => {
   if (option.canBeShown) canBeShown = option.canBeShown();
   return canBeShown;
 };
+//Outputs the current interaction indicated by the route $npcInteractionRoute and directed to the Npc in $npc
+//Not recommended to use it directly unless you know exactly what are you doing, use openNpcInteraction macro instead.
 Macro.add("npcInteraction", {
   handler: function () {
     let vars = variables() as any;
@@ -265,6 +321,8 @@ Macro.add("npcInteraction", {
     $(document.createElement("span")).wiki(result).appendTo(this.output);
   },
 });
+//Outputs a text based on the current person uniqueness in $npc or passed as the first parameter
+//Uses the tables in the UniquenessTables class.
 Macro.add("personUniqueness", {
   handler: function () {
     let person: Person = this.args[1] || Variables().npc;

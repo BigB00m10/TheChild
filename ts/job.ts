b33bedd6e89e83c09ff6dc70c7d08654c7d737b8
outch <joeffrey.collin@gmaiL;com>
2023-01-17 13:28:23 +0100

    i did the pc mastuber part - added porn hub logo - added 2 video gifts - added a cum end video gift -------------------------------------------------- -------- - added a teacher job

--- ts/job.ts
+++ ts/job.ts
@@ -1,22 +1,19 @@
 interface Job {
   name: string;
   pay: number;
-  enterTime: string;//Not exactly the enter time but the time that the player absolutely needs to go. Forced by unlessEmergency widget.
+  enterTime: string; //Not exactly the enter time but the time that the player absolutely needs to go. Forced by unlessEmergency widget.
+  workHours: number; //How many hours this job will take. More hours means more energy depleted too.
 }
 let Jobs: Record<string, Job> = {
   garbageCollector: {
     name: "garbage collector",
-    pay: Math.round((800 / 21.74) * 100) / 100,
+    pay: 37.38, //Math.round((800 / 21.74) * 100) / 100, //800 a month divided by the average days per month with two decimal places
     enterTime: "8:30 AM",
+    workHours: 8,
   },
   Teacher: {
     name: "Teacher shool",
     pay: Math.round((1000 / 11.14) * 100) / 100,
     enterTime: "8:00 AM",
   },
-  Delivery: {
-    name: "Pizza delivery man",
-    pay: Math.round((350 / 14.28) * 100) / 100,
-    enterTime: "10:30 AM",
-  }, 
 };

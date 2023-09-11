5d0c5d21ee1d64df547d316b36c68ce1e5bd81b1
Carrabina <zakopako@mail.com>
2023-09-11 23:28:27 +0200

    * In the mall added: cinema, restaurant where you can eat to recover energy, market directly linked for the moment to the pc store, sex shop, pet store * Added an alley that will have 2 options (prostitute your slave and speak with a dealer to buy drugs) * For school if we want to kidnap a child it's in the playground now in the school * Created the parts the images are missing, added the jobs teacher, in the director's office you can apply for the Job teacher with a scenario and accept the jobs or not without modifying in the settings

--- ts/job.ts
+++ ts/job.ts
@@ -1,19 +1,22 @@
 interface Job {
   name: string;
   pay: number;
-  enterTime: string; //Not exactly the enter time but the time that the player absolutely needs to go. Forced by unlessEmergency widget.
-  workHours: number; //How many hours this job will take. More hours means more energy depleted too.
+  enterTime: string;
+  workHours: number;
 }
+
 let Jobs: Record<string, Job> = {
   garbageCollector: {
     name: "garbage collector",
-    pay: 37.38, //Math.round((800 / 21.74) * 100) / 100, //800 a month divided by the average days per month with two decimal places
+    pay: 37.38,
     enterTime: "8:30 AM",
     workHours: 8,
   },
-  //Teacher: {
-  //  name: "Teacher shool",
-  //  pay: Math.round((1000 / 11.14) * 100) / 100,
-  //  enterTime: "8:00 AM",
-  //},
+  teacher: {
+    name: "Teacher",
+    pay: 45.67,
+    enterTime: "9:00 AM",
+    workHours: 7,
+  },
 };
+ 
